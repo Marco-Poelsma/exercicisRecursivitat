a8fa440288090f30c7f4e2a3337b5ddd391f766e
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2025-02-24 09:02:12 +0100

    Valor mínim d'un vector

--- Main.java
+++ Main.java
@@ -13,7 +13,12 @@ public class Main {
         System.out.println(sumaDigitos(n) + "\n");
 
         System.out.println(multi(arr, 0) + "\n");
+
+        int[][] mat = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(sumaMat(mat, 0, 0) + "\n");
         
+        int[] vector = {4, 2, 5, 1, 3, 6, 6};
+        System.out.println(minimVector(vector, 0));
     }
 
     public static int sum(int[] arr, int pos) {
@@ -44,5 +49,26 @@ public class Main {
             return 1;
         }
         return arr[pos] * multi(arr, pos + 1);
+    }
+
+    public static int sumaMat(int[][] mat, int posX, int posY) {
+        if (posX == mat.length) {
+            return 0;
+        }
+        if (posY == mat[posX].length) {
+            return sumaMat(mat, posX + 1, 0);
+        }
+        return mat[posX][posY] + sumaMat(mat, posX, posY + 1);
+    }
+
+    public static int minimVector(int[] vector, int pos) {
+        if (pos < vector.length) {
+            int min = minimVector(vector, pos + 1);
+            if (vector[pos] < min) {
+                return vector[pos];
+            }
+            return min;
+        }
+        return Integer.MAX_VALUE;
     }
 }
