d44db592e3337aab0cc32746b225c362e0cd1d92
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2025-02-24 08:53:14 +0100

    Divisió recursiva

--- Main.java
+++ Main.java
@@ -2,7 +2,12 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4};
 
-        System.out.println(sum(arr, 0));
+        System.out.println(sum(arr, 0)+"\n");
+
+        int n = 1;
+        int d = 1;
+
+        System.out.println(div(n, d));
     }
 
     public static int sum(int[] arr, int pos) {
@@ -11,5 +16,13 @@ public class Main {
         }
         return arr[pos] + sum(arr, pos + 1);
         
+    }
+
+    public static int div(int n, int d) {
+        if (n < d) {
+            System.out.println("Residu:" + n);
+            return 0;
+        }
+        return 1 + div(n - d, d);
     }
 }
